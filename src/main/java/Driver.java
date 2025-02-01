e78eec080f4f3fa26101e6bf8bf97a0e086b6221
pattycrowder <pattycrowder@pattys-mbp.lan>
2025-02-01 14:25:07 -0800

    handles command line arguments

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,8 +1,18 @@
 public class Driver {
 
 	public static void main(String[] args) {
+		if(args.length == 0) {
+			System.out.println("Please provide an expression toe valuate");
+			return;
+		}
+		
+		String expression = String.join(" ", args);
+		evaluate(expression);
+	}
+	
+	public static void evaluate(String expression) {
 		Output output = new Output();
-		System.out.println(output.getResult("(2 + 2"));
+		System.out.println(output.getResult(expression));
 	}
 
 }
